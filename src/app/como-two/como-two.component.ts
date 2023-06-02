1e8527807ecea805fb2af252a69ad7d6f97361b4
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2023-06-02 07:36:29 -0500

    Feat: 02-06-2023 - Suscripción de un segundo componente a observable.
    Suscripción de un segundo componente para ver el funcionamiento de un observable.

--- src/app/como-two/como-two.component.ts
+++ src/app/como-two/como-two.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyObservableService } from '../observable/my-observable.service';
 
 @Component({
 	selector: 'app-como-two',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 
 export class ComoTwoComponent {
+	mySubscription: any = null;
+	myObservableResponse: number[] = [];
+
+	constructor(
+		private myObservableService: MyObservableService
+	) {
+
+	}
+
+	xSuscribe(): void {
+		if(this.mySubscription == null) {
+			this.mySubscription = this.myObservableService.myObservableChanges$().subscribe({
+				next: (response: number[]) => {
+					this.myObservableResponse = JSON.parse(JSON.stringify(response));
+				}
+			});
+		}
+	}
+
+	xUnsuscribe(): void {
+		this.mySubscription.unsubscribe();
+
+		this.mySubscription = null;
+	}
 }
